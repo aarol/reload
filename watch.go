75d42c879bd38132bddbae965f0c4fee0d09c65c
aarol <aarol@iki.fi>
2023-10-14 15:00:03 +0300

    ignore closed events

--- watch.go
+++ watch.go
@@ -2,7 +2,6 @@ package reload
 
 import (
 	"errors"
-	"fmt"
 	"io/fs"
 	"log"
 	"path"
@@ -29,10 +28,9 @@ func WatchDirectories(directories []string) {
 	for _, path := range directories {
 		directories, err := recursiveWalk(path)
 		if err != nil {
-			fmt.Printf("%T\n", err)
 			var pathErr *fs.PathError
 			if errors.As(err, &pathErr) {
-				Log.Printf("directory doesn't exist: %s", pathErr.Path)
+				Log.Printf("directory doesn't exist: %s\n", pathErr.Path)
 			} else {
 				Log.Printf("error walking directories: %s\n", err)
 			}
@@ -66,15 +64,9 @@ func reloadDedup(w *fsnotify.Watcher) {
 
 	for {
 		select {
-		case err, ok := <-w.Errors:
-			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
-				return
-			}
+		case err := <-w.Errors:
 			Log.Println("error watching: ", err)
-		case e, ok := <-w.Events:
-			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
-				return
-			}
+		case e := <-w.Events:
 			switch {
 			case e.Has(fsnotify.Create):
 				// Watch any created file/directory
@@ -88,15 +80,8 @@ func reloadDedup(w *fsnotify.Watcher) {
 				timer.Reset(wait)
 				lastEdited = path.Base(e.Name)
 
-			case e.Has(fsnotify.Rename):
+			case e.Has(fsnotify.Rename), e.Has(fsnotify.Remove):
 				// a renamed file might be outside the specified paths
-				directories, _ := recursiveWalk(e.Name)
-				for _, v := range directories {
-					w.Remove(v)
-				}
-				w.Remove(e.Name)
-
-			case e.Has(fsnotify.Remove):
 				directories, _ := recursiveWalk(e.Name)
 				for _, v := range directories {
 					w.Remove(v)
